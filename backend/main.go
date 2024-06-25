052ad8a612d1f5fb3d2f15f48f7802b0c975f02b
Adam Browner-Denning <adamjbrowner@gmail.com>
2024-06-25 14:55:55 +0100

    Added createToDoHanlder func, added switch to main to handle different methods

--- backend/main.go
+++ backend/main.go
@@ -19,8 +19,14 @@ func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
 
-		getToDoListHandler(w, r)
-
+		switch r.Method {
+		case "GET":
+			getToDoListHandler(w, r)
+		case "POST":
+			createToDoHandler(w, r)
+		default:
+			http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
+		}
 	})
 
 	fmt.Println("Server is running on port 8080")
@@ -30,4 +36,20 @@ func main() {
 func getToDoListHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(toDoList)
+}
+
+func createToDoHandler(w http.ResponseWriter, r *http.Request) {
+	var newToDo ToDo
+
+	dec := json.NewDecoder(r.Body)
+	err := dec.Decode(&newToDo)
+
+	if err != nil {
+		http.Error(w, "Invalid Input", http.StatusBadRequest)
+		return
+	}
+
+	toDoList = append(toDoList, newToDo)
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(newToDo)
 }
